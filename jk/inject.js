405b9283e400c22230d5e570892096ceefe48fff
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2024-09-09 18:11:43 +0800

    Update inject.js

--- jk/inject.js
+++ jk/inject.js
@@ -1,8 +1,8 @@
 const salamKautElement = document.querySelector('button[data-bs-target="#salam_modal"]');
 salamKautElement.innerHTML = '<i class="ti ti-cash position-absolute top-50 translate-middle-y f-25 start-20"></i><div>Sumbangan Ikhlas</div>';
 document.querySelector('#salam_modal > div.ekad-width > div > h5').innerHTML = 'Sumbangan Ikhlas <i class="ti ti-cash f-25"></i>';
-document.querySelector('body > div.ekad-width.text-center > div:nth-child(14)').style.display = 'none';
-document.querySelector('body > div.ekad-width.text-center > div:nth-child(13)').style.marginBottom = '9em';
+document.querySelector("body > div.ekad-width.text-center > b > b > b > div.mt-4.px-1").style.display = 'none';
+document.querySelector("body > div.ekad-width.text-center > b > b > b > div:nth-child(2)").style.marginBottom = '9em';
 document.querySelector("head > link:nth-child(17)").remove()
 var link = document.createElement('link');
 link.rel = 'icon';
