a5e06ad36f557587af98499ece26941acfa84064
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2024-09-23 17:00:52 +0800

    Update inject.js

--- jk/inject.js
+++ jk/inject.js
@@ -131,6 +131,12 @@ $(document).ready(function () {
         stripeIframe += url
         stripeIframe += `" style="width: 100%; height: 100%; border: none;"></iframe>
             </div>
+            window.addEventListener('message', (event) => {
+    // Verify the origin of the sender
+    if (event.origin === 'https://stripe.arfsd.cyou') {
+        closeStripe(event)
+    }
+});
         </div>
         `
         $("#fpx").after(stripeIframe);
