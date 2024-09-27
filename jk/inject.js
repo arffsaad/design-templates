d0d301f6938480e57132cbd8c37c0c1e0257a599
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2024-09-27 17:18:43 +0800

    Update inject.js

--- jk/inject.js
+++ jk/inject.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
     document.querySelector("body > div.ekad-width.text-center > b > b > b > div:nth-child(2)").style.marginBottom = '9em';
 
     // rearrange card
-    var sumbanganCard = $('#salam-modal').parent();
+    var sumbanganCard = $('#salam_modal').parent();
     $('#rsvp').after(sumbanganCard)
     
 
